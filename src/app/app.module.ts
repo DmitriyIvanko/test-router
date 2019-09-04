3001c80566322e499838154ca5edd58c784196a1
DmitriyIvanko <ivanko.dmitriy@gmail.com>
2019-09-04 15:39:34 +0300

    + temp save;

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { Router } from '@angular/router';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { CrisisListComponent } from './crisis-list/crisis-list.component';
-import { HeroListComponent } from './hero-list/hero-list.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HeroesModule } from './heroes/heroes.module';
@@ -12,15 +12,22 @@ import { HeroesModule } from './heroes/heroes.module';
   declarations: [
     AppComponent,
     CrisisListComponent,
-    HeroListComponent,
     PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
+    HeroesModule,
     AppRoutingModule,
-    HeroesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(
+    router: Router,
+  ) {
+    const replacer = (key, value) => (typeof value === 'function') ? value.name : value;
+
+    console.log('Routes: ', JSON.stringify(router.config, replacer, 2));
+  }
+}
--- src/app/heroes/hero-detail/hero-detail.component.ts
+++ src/app/heroes/hero-detail/hero-detail.component.ts
@@ -1,18 +1,33 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { Observable } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+
 import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
-  styleUrls: ['./hero-detail.component.css']
+  styleUrls: ['./hero-detail.component.scss']
 })
 export class HeroDetailComponent implements OnInit {
 
-  @Input() hero: Hero;
+  @Input()
+  public hero$: Observable<Hero>;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private service: HeroService,
+  ) { }
 
-  ngOnInit() {
+  public ngOnInit(): void {
+    this.hero$ = this.route.paramMap.pipe(
+      switchMap((params: ParamMap) => {
+        return this.service.getHero(+params.get('id'));
+      }),
+    );
   }
 
 }
